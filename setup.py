40cb2bb0662edda9948a11f850ab6e72db560a4e
JeroenDM <jeroendemaeyer@live.be>
2019-09-17 09:47:56 +0200

    update version and rewrite setup.py file

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from Cython.Build import cythonize
 
 extensions = [Extension("acrolib.cost_functions", ["src/acrolib/cost_functions.pyx"])]
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="acrolib",
-    version="0.0.3",
+    version="0.0.4",
     author="Jeroen De Maeyer",
     author_email="jeroen.demaeyer@kuleuven.be",
     description="General utilities for my robotics research at ACRO.",
@@ -16,7 +19,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/JeroenDM/acrolib",
     packages=find_packages(where="src"),
-    install_requires=["cython", "numpy", "pyquaternion"],
+    install_requires=requirements,
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
